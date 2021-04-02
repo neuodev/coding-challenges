b54f416f1faae85e440f6d83ab0be6095bf63755
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-02 09:25:46 +0200

    Appling two sum using  Hash Table

--- towSum.ts
+++ towSum.ts
@@ -2,7 +2,9 @@
 // return vlaue of [3,4]
 
 // Itrative
-function towSumIterative(array: number[], target) {
+// time: O(n^2)
+// space: O(1)
+function towSumIterative(array: number[], target: number) {
   for (let p1 = 0; p1 < array.length; p1++) {
     const element1 = array[p1];
     for (let p2 = p1; p2 < array.length; p2++) {
@@ -15,3 +17,23 @@ function towSumIterative(array: number[], target) {
 }
 
 console.log(towSumIterative([1, 3, 7, 9, 2], 5));
+
+// Using Hash Table
+// time: O(n)
+// space: O(n) -> due to hash table 
+function twoSumHash(array: number[], target: number) {
+  let hashTable = {};
+
+  for (let p = 0; p < array.length; p++) {
+    const element = array[p];
+
+    if (hashTable[element] >= 0) return [hashTable[element], p];
+
+    let numberToFind = target - element;
+    hashTable[numberToFind] = p;
+  }
+
+  return null;
+}
+
+console.log(twoSumHash([1, 3, 7, 9, 2], 11));
