c647ee795849eecd12e42fa487e77e261c8330ef
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-10 11:31:50 +0200

    Solve Branch sums

---
+++ Branch_Sum.ts
@@ -0,0 +1,15 @@
+function branchSum(root) {
+  let sums = [];
+  calcBranchSum(root, 0, sums);
+  return sums;
+}
+
+function calcBranchSum(node, sum, results) {
+  if (!node) {
+    results.push(sum);
+    return;
+  }
+  sum += node.val;
+  calcBranchSum(node.left, sum, results);
+  calcBranchSum(node.right, sum, results);
+}
