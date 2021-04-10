97263de210d52b0c6ef14b9e565c427c6f722947
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-10 17:02:49 +0200

    Slove Insetion sort

---
+++ InsertionSort.ts
@@ -0,0 +1,19 @@
+function insetionSort(array: number[]) {
+  for (let i = 0; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j--;
+    }
+  }
+
+  return array;
+}
+
+function swap(j: number, i: number, array: number[]) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+insetionSort([1, 9, 3, 4]);
