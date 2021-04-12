e6c0153aa48ab51a9860332693b38bf8c90ffff7
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-12 19:07:16 +0200

    Validate BST

---
+++ ValidateBST.ts
@@ -0,0 +1,42 @@
+import BST from './BSTConstruction';
+const bst = new BST();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+bst.insert(1);
+bst.insert(5);
+bst.insert(13);
+bst.insert(22);
+bst.insert(14);
+bst.insert(12);
+
+function validateBST(root, parent = null, type) {
+  if (!root) return;
+
+  if (type == 'left' && root.data > parent.data) {
+    return false;
+  }
+  if (type == 'right' && root.data < parent.data) {
+    return false;
+  }
+  validateBST(root.left, root, 'left');
+  validateBST(root.right, root, 'right');
+
+  return true;
+}
+console.log(validateBST(bst.root));
+
+const validateBST = function (root) {
+  return validateBSTHelper(root, -Infinity, Infinity);
+};
+
+function validateBSTHelper(tree, minValue, maxValue) {
+  if (!tree) return true;
+  if (tree.data < minValue || tree.data >= maxValue) return false;
+  return (
+    validateBSTHelper(tree.left, minValue, tree.data) &&
+    validateBSTHelper(tree.right, tree.data, maxValue)
+  );
+}
+validateBST(bst.root);
