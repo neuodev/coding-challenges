532eba9c06906e4ae4b92904a8c704af5a8903d8
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-13 21:48:31 +0200

    Get the powerset of and array of integers

---
+++ PowerSet.ts
@@ -0,0 +1,16 @@
+function getPowerset(array) {
+  let powerSet = [[]];
+  if (array.length === 0) return powerSet;
+  for (let i = 0; i < array.length; i++) {
+    let setLen = powerSet.length - 1;
+    while (setLen >= 0) {
+      let setCopy = [...powerSet[setLen]];
+      setCopy.push(array[i]);
+      powerSet.push(setCopy);
+      setLen--;
+    }
+  }
+  console.log(powerSet);
+}
+
+getPowerset([1, 2, 3]);
