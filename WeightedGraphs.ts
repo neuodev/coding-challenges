a5182e303f054687837ad4ae299e4ea6dbacbf3b
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-13 12:34:10 +0200

    add the Edge class

--- WeightedGraphs.ts
+++ WeightedGraphs.ts
@@ -1,6 +1,15 @@
 class Vertix {
+  neighbours: Edge[];
   constructor(public label: string) {
     this.label = label;
+    this.neighbours = [];
   }
 }
 
+class Edge {
+  constructor(public from: Vertix, public to: Vertix, public weight: number) {
+    this.from = from;
+    this.to = to;
+    this.weight = weight;
+  }
+}
