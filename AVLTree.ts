4ef2c4df5f0d3979c9cf9c3ca56e9f7e3400e7f5
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-13 08:13:53 +0200

    Add the insert method in the AVLTree

--- AVLTree.ts
+++ AVLTree.ts
@@ -16,5 +16,20 @@ class AVLTree {
     this.root = null;
   }
 
+  //  10
+  //     20
+  //        30
 
+  insert(val: number) {
+    this.root = this._insert(this.root, val);
+  }
+
+  _insert(node, val) {
+    if (node === null) return new AVLNode(val);
+    if (val < node.val) {
+      node.left = this._insert(node.left, val);
+    } else node.right = this._insert(node.right, val);
+
+    return node;
+  }
 }
