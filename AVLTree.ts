bf43b2a509742ccd4894dd64f9698bf3daa32c7e
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-13 08:06:08 +0200

    Build the AVLNode class

---
+++ AVLTree.ts
@@ -0,0 +1,11 @@
+class AVLNode {
+  left: AVLNode;
+  right: AVLNode;
+  height: number;
+  constructor(public value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.height = 0;
+  }
+}
