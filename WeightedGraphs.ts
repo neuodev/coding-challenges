ee9a487ce003ce87dbc6d0f154efacc02a425bc0
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-13 19:58:26 +0200

    Test the Graph

--- WeightedGraphs.ts
+++ WeightedGraphs.ts
@@ -17,3 +17,36 @@ class Edge {
     this.weight = weight;
   }
 }
+
+class Graph {
+  graph = {};
+
+  addNode(value) {
+    this.graph[value] = new Vertix(value);
+  }
+
+  addEdge(from: string, to: string, weight: number) {
+    if (from == to || !this.graph[from] || !this.graph[to]) return false;
+    this.graph[from].addEdge(this.graph[to], weight);
+    this.graph[to].addEdge(this.graph[from], weight);
+  }
+
+  print() {
+    let output = new Set();
+    for (const node in this.graph) {
+      for (const edge of this.graph[node].neighbours) {
+        output.add(`${edge.from.label} -> ${edge.to.label} W:${edge.weight}`);
+      }
+    }
+    console.log(output);
+  }
+}
+
+let graph = new Graph();
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addEdge('A', 'B', 2);
+graph.addEdge('A', 'C', 2);
+graph.print();
+console.log(graph.graph);
