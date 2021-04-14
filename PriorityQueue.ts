5cb5c587356d47a9f9f7226dceba9bd4c1730158
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-14 11:54:09 +0200

    Add the bubble up and the enqueue methods

--- PriorityQueue.ts
+++ PriorityQueue.ts
@@ -3,4 +3,26 @@ class PriorityQueue {
   constructor() {
     this.values = [];
   }
+
+  enqueue(val: string, priority: number) {
+    this.values.push({ val, priority });
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    let ele = this.values[idx];
+
+    while (idx > 0) {
+      let parentIdx = this._parent(idx);
+      let parent = this.values[parentIdx];
+      if (ele.priority >= parent.priority) break;
+      this.values[parentIdx] = ele;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  _parent(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
 }
