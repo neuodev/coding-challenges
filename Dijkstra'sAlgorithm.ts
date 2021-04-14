afd4d2a4a211df66f8ef888f97fe929bf3f95553
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-14 12:33:32 +0200

    Build new Graph

--- Dijkstra'sAlgorithm.ts
+++ Dijkstra'sAlgorithm.ts
@@ -134,3 +134,17 @@ class Graph {
     console.log(output);
   }
 }
+
+const graph = new Graph();
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addNode('D');
+graph.addNode('E');
+graph.addEdge('A', 'B', 3);
+graph.addEdge('A', 'D', 2);
+graph.addEdge('A', 'C', 4);
+graph.addEdge('B', 'E', 1);
+graph.addEdge('B', 'D', 6);
+graph.addEdge('D', 'E', 5);
+graph.addEdge('D', 'C', 1);
