6f9dee47566d3411dd445ebc1369c632da71856b
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-16 01:29:53 +0200

    Build queue using a stack

--- QueueWithStacks.ts
+++ QueueWithStacks.ts
@@ -1,8 +1,10 @@
 class Queue {
+  queue = [];
   s1 = [];
   s2 = [];
 
   enqueue(value: number) {
+    this.queue.push(value);
     this.s1.push(value);
   }
   dequeue() {
@@ -13,14 +15,23 @@ class Queue {
     }
     return this.s2.pop();
   }
+
+  isEmpty() {
+    return this.s1.length === 0 && this.s2.length === 0;
+  }
+
+  peek() {
+    return this.queue[0];
+  }
 }
 
 const queue = new Queue();
 queue.enqueue(1);
 queue.enqueue(2);
 queue.enqueue(3);
-console.log(queue.s1);
 
+console.log(queue.peek());
 console.log(queue.dequeue());
 console.log(queue.dequeue());
 console.log(queue.dequeue());
+console.log(queue.isEmpty());
