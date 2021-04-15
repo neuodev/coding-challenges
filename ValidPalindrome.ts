bb4d3bd4ee6a8f2ea03cade628a3015f4e3f379d
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-15 15:45:01 +0200

    test is string is palindrome

---
+++ ValidPalindrome.ts
@@ -0,0 +1,18 @@
+function isPalindrome(string: string) {
+  let newString = '';
+  for (const char of string.toLocaleLowerCase()) {
+    if (char.charCodeAt(0) > 122 || char.charCodeAt(0) < 97 || char == ' ')
+      continue;
+    newString += char;
+  }
+  let left = 0,
+    right = newString.length - 1;
+  while (left < right) {
+    if (newString[left] !== newString[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
