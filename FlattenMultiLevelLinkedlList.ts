bcd7e75da94bb2e1c04102dd3960b0d5a5c6db22
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-15 23:16:30 +0200

    Flatten Linked List

--- FlattenMultiLevelLinkedlList.ts
+++ FlattenMultiLevelLinkedlList.ts
@@ -5,6 +5,27 @@ type ListNode = {
   child: ListNode;
 };
 
-function flatenLinkedList(head: ListNode) {
-    
+function flattenLinkedList(head: ListNode) {
+  if (head == null) return head;
+  let current = head;
+  while (current !== null) {
+    if (current.child) {
+      let next = current.next;
+      let tail = current.child;
+      while (tail.next !== null) {
+        tail = tail.next;
+      }
+      current.next = current.child;
+      current.next.prev = current;
+      current.child = null;
+      tail.next = next;
+      if (next !== null) {
+        next.prev = tail;
+      }
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
 }
