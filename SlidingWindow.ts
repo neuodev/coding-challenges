906880a3a7f23af5cc3149d076232a51cdd842fd
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-15 14:55:39 +0200

    Slideing Window

--- SlidingWindow.ts
+++ SlidingWindow.ts
@@ -1,25 +1,18 @@
 function substring(string: string) {
   if (string.length <= 1) return string.length;
-  const hashMap = {};
+  const seenChars = {};
   let longest = 0;
-  let currentLong = 0;
   let left = 0;
-  let right = 0;
-  for (let i = 0; i < string.length; i++) {
-    let char = string[i];
-    if (!hashMap[char]) {
-      hashMap[char] = i;
-      right = i;
-      currentLong++;
-    } else {
-      if (hashMap[char] < left) {
-        hashMap[char] = i;
-      } else {
-        left = hashMap[char] + 1;
-      }
-      longest = longest > currentLong ? longest : currentLong;
+  for (let right = 0; right < string.length; right++) {
+    const currentChar = string[right];
+    const prevSeenChar = seenChars[currentChar];
+    if (prevSeenChar >= left) {
+      left = prevSeenChar + 1;
     }
+    seenChars[currentChar] = right;
+    longest = Math.max(longest, right - left + 1);
   }
+
   return longest;
 }
-console.log(substring('abcbda'));
+console.log(substring('abcabcbb'));
