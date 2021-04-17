b72fb2e6295ccf47246744fe5faecc03ada140db
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-17 13:33:05 +0200

    Count the number of Islands

---
+++ NumberOfIslands.ts
@@ -0,0 +1,38 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == 1) {
+        count += 1;
+        callBFS(grid, i, j);
+      }
+    }
+  }
+
+  return count;
+};
+
+function callBFS(grid, i, j) {
+  // check out bounders
+
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[i].length ||
+    grid[i][j] == 0
+  )
+    return;
+
+  // mark the island you visited as 0
+  grid[i][j] = 0;
+  // search all it's bounders
+  callBFS(grid, i + 1, j); // up
+  callBFS(grid, i - 1, j); // down
+  callBFS(grid, i, j - 1); // left
+  callBFS(grid, i, j + 1); // right
+}
