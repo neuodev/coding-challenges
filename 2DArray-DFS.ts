f5c3fcb93cd124700154a4506ec196a4ca6e07aa
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-17 13:02:38 +0200

    DFS of 2D Array

--- 2DArray-DFS.ts
+++ 2DArray-DFS.ts
@@ -1,3 +1,27 @@
+function Traverse(matrix: number[][]) {
+  let visited = new Set();
+  DFS(visited, matrix, 0, 0);
+  console.log(visited);
+  return visited;
+}
+
+function DFS(visited, grid, i, j) {
+  if (
+    i < 0 ||
+    j < 0 ||
+    i >= grid.length ||
+    j >= grid[i].length ||
+    visited.has(grid[i][j])
+  )
+    return;
+
+  visited.add(grid[i][j]);
+  DFS(visited, grid, i - 1, j); // up
+  DFS(visited, grid, i, j + 1); // right
+  DFS(visited, grid, i + 1, j); // bottom
+  DFS(visited, grid, i, j - 1); // left
+}
+
 function buildMatrix(row: number, col: number) {
   // build the grid
   let grid = [];
@@ -11,4 +35,6 @@ function buildMatrix(row: number, col: number) {
 
     grid.push(row);
   }
+
+  return grid;
 }
